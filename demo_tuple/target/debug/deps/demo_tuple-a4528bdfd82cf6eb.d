584666277d1e3dc03c40f23e26a1dca53dba7a9b
刘泽锦 <best_liuzejin@sina.com>
2022-01-18 22:38:05 +0800

    add: enum

---
+++ demo_tuple/target/debug/deps/demo_tuple-a4528bdfd82cf6eb.d
@@ -0,0 +1,5 @@
+/Users/liuzejin/Leason/2022/rust/learn_rust/demo_tuple/target/debug/deps/demo_tuple-a4528bdfd82cf6eb.rmeta: src/main.rs
+
+/Users/liuzejin/Leason/2022/rust/learn_rust/demo_tuple/target/debug/deps/demo_tuple-a4528bdfd82cf6eb.d: src/main.rs
+
+src/main.rs:
---
+++ demo_tuple/target/debug/deps/demo_tuple-e8aa40d0f243c899.d
@@ -0,0 +1,5 @@
+/Users/liuzejin/Leason/2022/rust/learn_rust/demo_tuple/target/debug/deps/demo_tuple-e8aa40d0f243c899.rmeta: src/main.rs
+
+/Users/liuzejin/Leason/2022/rust/learn_rust/demo_tuple/target/debug/deps/demo_tuple-e8aa40d0f243c899.d: src/main.rs
+
+src/main.rs:
